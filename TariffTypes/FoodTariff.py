2583d77e6b37696b2227b76c9008d4e0d45fc627
Nikita <tomchukn4@gmail.com>
2025-03-19 22:13:45 +0700

    FIX RoomTariff.py FoodTariff.py

--- TariffTypes/FoodTariff.py
+++ TariffTypes/FoodTariff.py
@@ -1,28 +1,25 @@
+from nltk.data import retrieve
+
+
 class FoodTariff:
 
-    def __init__(self, food_tariff: str, people_count: str):
+    def __init__(self, food_tariff: str, people_count: int):
         self.__food_tariff = food_tariff
 
         try:
-            self.__people_count = int(people_count)
-
-            try:
-                if self.__food_tariff == "no":
-                    self.__price = 0
-                elif self.__food_tariff == "breakfast":
-                    self.__price = 280 * people_count
-                elif self.__food_tariff == "full":
-                    self.__price = 1000 * people_count
-                else:
-                    raise TypeError
-            except TypeError:
-                print("Wrong operation. No such food tariff.")
-
-        except ValueError:
-            print("Arguments Error. Can not convert people count into number.")
+            if self.__food_tariff == "no":
+                self.__food_price_per_day = 0
+            elif self.__food_tariff == "breakfast":
+                self.__food_price_per_day = 280 * people_count
+            elif self.__food_tariff == "full":
+                self.__food_price_per_day = 1000 * people_count
+            else:
+                raise TypeError
+        except TypeError:
+            print("Wrong operation. No such food tariff.")
 
     def __repr__(self):
         return self.__food_tariff
 
-    def get_price(self):
-        return self.__price
+    def get_food_price_per_day(self):
+        return self.__food_price_per_day
--- TariffTypes/RoomTariff.py
+++ TariffTypes/RoomTariff.py
@@ -1,21 +1,19 @@
 class RoomTariff:
 
-    def __init__(self, room_type: str):
-        self.__room_type = room_type
-        try:
-            if room_type == "standart":
-                self.__price_scale = 1
-            elif room_type == "up_standart":
-                self.__price_scale = 1.2
-            elif room_type == "apartments":
-                self.__price_scale = 1.5
-            else:
-                raise TypeError
-        except TypeError:
+    def __init__(self, room_tariff: str):
+        self.__room_tariff = room_tariff
+
+        if room_tariff == "standart":
+            self.__price_scale = 1
+        elif room_tariff == "up_standart":
+            self.__price_scale = 1.2
+        elif room_tariff == "apartments":
+            self.__price_scale = 1.5
+        else:
             print("Wrong operation. No such room type.")
 
     def __repr__(self):
-        return self.__room_type
+        return self.__room_tariff
 
     def get_price_scale(self):
         return self.__price_scale
