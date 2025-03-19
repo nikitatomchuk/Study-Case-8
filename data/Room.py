eaebb7aa7820d72f092cfe9756db3b6fbfa7e69c
Nikita <tomchukn4@gmail.com>
2025-03-19 22:14:02 +0700

    ADD Room.py

---
+++ data/Room.py
@@ -0,0 +1,22 @@
+from TariffTypes.RoomTariff import RoomTariff
+from TariffTypes.RoomType import RoomType
+
+
+class Room:
+
+    def __init__(self, room_type: RoomType, room_tariff: RoomTariff,
+                 room_number: int, days_count: int, max_people_count: int):
+
+        self.__room_number = room_number
+        self.__days_count = days_count
+        self.__max_people_count = max_people_count
+        self.__room_price_per_day = room_tariff.get_price_scale() * room_type.get_size_price_per_day()
+
+    def get_room_number(self):
+        return self.__room_number
+
+    def get_room_price(self):
+        return self.__room_price_per_day * self.__days_count
+
+    def get_max_people_count(self):
+        return self.__max_people_count
