e285ca944613e4cdbab6afe875789b6c918db86b
Nikita <tomchukn4@gmail.com>
2025-03-21 05:06:24 +0700

    FIX Room.py RoomsDataBase.py

--- data/Room.py
+++ data/Room.py
@@ -6,28 +6,42 @@ from datetime import date
 class Room:
 
     def __init__(self, room_type: RoomType, room_tariff: RoomTariff, room_number: int, max_people_count: int):
-        self.__free_date = date.min
+        self.__busy_dates = {}
         self.__room_number = room_number
         self.__max_people_count = max_people_count
         self.__room_price_per_day = room_tariff.get_price_scale() * room_type.get_size_price_per_day()
+        self.__discount_price = self.__room_price_per_day * 0.7
 
     def __repr__(self):
         return str([self.__room_number, self.__max_people_count, self.__room_price_per_day])
 
-    def get_free_date(self):
-        return self.__free_date
+    def get_busy_dates(self):
+        return self.__busy_dates
 
-    def set_free_date(self, free_date: date):
-        self.__free_date = free_date
+    def set_busy_date(self, busy_date_start: date, busy_date_end: date):
+        self.__busy_dates.update({busy_date_start: busy_date_end})
 
-    def get_room_number(self):
+    def get_number(self):
         return self.__room_number
 
-    def get_room_price(self):
+    def get_price(self):
         return self.__room_price_per_day
 
     def get_max_people_count(self):
         return self.__max_people_count
 
-    def is_free(self, target_date: date):
-        return self.__free_date <= target_date
+    def is_free(self, book_start_date: date, book_end_date: date):
+        if len(self.__busy_dates) == 0:
+            return True
+
+        for book_start in self.__busy_dates.keys():
+            if book_start <= book_start_date <= self.__busy_dates[book_start]:
+                return False
+            elif book_start <= book_end_date <= self.__busy_dates[book_start]:
+                return False
+            elif book_start_date < book_start and self.__busy_dates[book_start] < book_end_date:
+                return False
+        return True
+
+    def get_discount_price(self):
+        return self.__discount_price
--- data/RoomsDataBase.py
+++ data/RoomsDataBase.py
@@ -22,21 +22,24 @@ class RoomDataBase:
     def get_all_rooms(self):
         return self.__all_rooms
 
+    def change_room_busy_date(self, room_number, book_start_day: date, book_end_day: date):
+        self.__all_rooms[room_number - 1].set_busy_date(book_start_day, book_end_day)
+
     def get_room_by_number(self, room_number: int):
         return self.__all_rooms[room_number - 1]
 
-    def get_free_rooms(self, target_date: date):
+    def get_free_rooms(self, book_start_date: date, book_end_date: date):
         self.__free_rooms = []
 
         for room in self.__all_rooms:
-            if room.is_free(target_date):
+            if room.is_free(book_start_date, book_end_date):
                 self.__free_rooms.append(room)
 
         return self.__free_rooms
 
     def get_free_room_by_number(self, room_number: int):
         for room in self.__free_rooms:
-            if room.get_room_number() == room_number:
+            if room.get_number() == room_number:
                 return room
 
         print("No rooms with such number are free.")
