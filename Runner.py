50c95b5c487845817f3d5b500e2c90ab1d1aaa93
Nikita <tomchukn4@gmail.com>
2025-03-20 14:09:01 +0700

    FIX Runner.py

--- Runner.py
+++ Runner.py
@@ -1,7 +1,10 @@
+from data.RoomsDataBase import RoomDataBase
 from handler.RequestHandler import RequestHandler
 
 
 def main():
+    rooms_data_base = RoomDataBase()
+    print(rooms_data_base.get_all_rooms())
     with open("handler/requests_data.txt", encoding='utf-8') as requests:
         for request in requests.readlines():
             request_data = RequestHandler(request.split())
