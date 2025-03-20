77017d2fe1a27f9cfb63e1bdc05435c91fea58c4
Nikita <tomchukn4@gmail.com>
2025-03-20 14:08:46 +0700

    FIX RoomTariff.py

--- TariffTypes/RoomTariff.py
+++ TariffTypes/RoomTariff.py
@@ -3,11 +3,11 @@ class RoomTariff:
     def __init__(self, room_tariff: str):
         self.__room_tariff = room_tariff
 
-        if room_tariff == "standart":
+        if room_tariff == "стандарт":
             self.__price_scale = 1
-        elif room_tariff == "up_standart":
+        elif room_tariff == "стандарт_улучшенный":
             self.__price_scale = 1.2
-        elif room_tariff == "apartments":
+        elif room_tariff == "апартамент":
             self.__price_scale = 1.5
         else:
             print("Wrong operation. No such room type.")
