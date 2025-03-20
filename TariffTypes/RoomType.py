d4da7e8e5fad82abab79a5982e6b302af9460d2c
Nikita <tomchukn4@gmail.com>
2025-03-20 14:08:53 +0700

    FIX RoomType.py

--- TariffTypes/RoomType.py
+++ TariffTypes/RoomType.py
@@ -3,13 +3,13 @@ class RoomType:
     def __init__(self, room_type: str, people_count: int):
         self.__room_type = room_type
 
-        if self.__room_type == "alone":
+        if self.__room_type == "одноместный":
             self.__size_price_per_day = 2900
-        elif self.__room_type == "duo":
+        elif self.__room_type == "двухместный":
             self.__size_price_per_day = 2300 * people_count
-        elif self.__room_type == "half-luxe":
+        elif self.__room_type == "полулюкс":
             self.__size_price_per_day = 3200 * people_count
-        elif self.__room_type == "luxe":
+        elif self.__room_type == "люкс":
             self.__size_price_per_day = 4100 * people_count
         else:
             print("Wrong operation. No such room size.")
