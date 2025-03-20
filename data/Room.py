3450ce5920fecb12b5d132172ba64c269f70f57b
Nikita <tomchukn4@gmail.com>
2025-03-20 13:58:23 +0700

    FIX Room.py RoomsDataBase.py

--- data/Room.py
+++ data/Room.py
@@ -1,13 +1,21 @@
 from TariffTypes.RoomTariff import RoomTariff
 from TariffTypes.RoomType import RoomType
+from datetime import date
 
 
 class Room:
 
     def __init__(self, room_type: RoomType, room_tariff: RoomTariff, room_number: int, max_people_count: int):
+        self.__free_date = date.min
         self.__room_number = room_number
         self.__max_people_count = max_people_count
         self.__room_price_per_day = room_tariff.get_price_scale() * room_type.get_size_price_per_day()
+
+    def get_free_date(self):
+        return self.__free_date
+
+    def set_free_date(self, free_date: date):
+        self.__free_date = free_date
 
     def get_room_number(self):
         return self.__room_number
@@ -17,3 +25,6 @@ class Room:
 
     def get_max_people_count(self):
         return self.__max_people_count
+
+    def is_free(self, target_date: date):
+        return self.__free_date <= target_date
--- data/RoomsDataBase.py
+++ data/RoomsDataBase.py
@@ -1,13 +1,15 @@
 from TariffTypes.RoomTariff import RoomTariff
 from TariffTypes.RoomType import RoomType
 from data.Room import Room
+from datetime import date
 
 
 class RoomDataBase:
 
     def __init__(self, rooms_data_path: str):
+        self.__free_rooms = []
         self.__all_rooms = []
-        with open("rooms_data.txt") as lines:
+        with open("rooms_data.txt", encoding='utf-8') as lines:
             for line in lines:
                 room_data = line.split()
                 room_number = int(room_data[0])
@@ -23,7 +25,18 @@ class RoomDataBase:
     def get_room_by_number(self, room_number: int):
         return self.__all_rooms[room_number - 1]
 
-    def get_free_rooms(self, date):
-        for room in self.__all_rooms:
-            pass
+    def get_free_rooms(self, target_date: date):
+        self.__free_rooms = []
 
+        for room in self.__all_rooms:
+            if room.is_free(target_date):
+                self.__free_rooms.append(room)
+
+        return self.__free_rooms
+
+    def get_free_room_by_number(self, room_number: int):
+        for room in self.__free_rooms:
+            if room.get_room_number() == room_number:
+                return room
+
+        print("No rooms with such number are free.")
