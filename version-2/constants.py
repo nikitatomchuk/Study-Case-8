0603f02332bf3610a4f81147b4f45f69cedae3c9
margosh1234r5563 <159226385+margosh1234r5563@users.noreply.github.com>
2025-03-29 19:39:12 +0700

    Update constants.py
    docstring

--- version-2/constants.py
+++ version-2/constants.py
@@ -2,11 +2,18 @@ import ru_local as ru
 
 
 class Files:
+    """
+    This class contains information about reservation files and hotel room files.
+    """
     DATA_FILE_NAME = "datafiles/rooms_data.txt"
     REQUESTS_FILE_NAME = "datafiles/requests_data.txt"
 
 
+
 class RoomTariffs:
+    """
+    This class contains information about hotel rates and upgrade coefficients.
+    """
     STANDARD = ru.STANDARD
     STANDARD_UP = ru.STANDARD_UP
     APARTMENT = ru.APARTMENT
@@ -17,6 +24,9 @@ class RoomTariffs:
 
 
 class RoomTypes:
+    """
+    This class contains information about the types of rooms and the price per person.
+    """
     ONE_PERSON = ru.ONE_PERSON
     TWO_PERSON = ru.TWO_PERSON
     HALF_LUXE = ru.HALF_LUXE
@@ -29,6 +39,9 @@ class RoomTypes:
 
 
 class FoodTariffs:
+    """
+    This class contains information about breakfast options and the price for them.
+    """
     NO_FOOD = ru.NO_FOOD
     BREAKFAST = ru.BREAKFAST
     FULL = ru.FULL
